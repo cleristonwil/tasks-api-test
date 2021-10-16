2dd02541d5424459195bdc9914aafad37e987005
cleriston <cleristonwil@gmail.com>
2021-10-16 18:38:02 -0300

    mudanca data api-test

--- src/test/java/br/ce/willian/tasks/apitest/APITest.java
+++ src/test/java/br/ce/willian/tasks/apitest/APITest.java
@@ -27,7 +27,7 @@ public class APITest {
 	@Test
 	public void deveAdicionarTarefaComSucesso() {
 		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2021-10-14\" }")
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2030-10-14\" }")
 			.contentType(ContentType.JSON)
 		.when()
 			.post("/todo")
